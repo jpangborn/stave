bbc15424f20efcd54859df0c0331413b951569db
Joshua Pangborn <joshua@pangborn.cloud>
2025-08-03 13:40:54 -0400

    Add presets to architecture tests

--- tests/Feature/ArchTest.php
+++ tests/Feature/ArchTest.php
@@ -7,6 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notification;
 use Livewire\Form;
 
+arch()->preset()->php();
+arch()->preset()->security();
+arch()->preset()->laravel();
+
 arch("globals")
     ->expect(["dd", "ddd", "die", "dump", "ray", "sleep", "eval", "env"])
     ->toBeUsedInNothing();
