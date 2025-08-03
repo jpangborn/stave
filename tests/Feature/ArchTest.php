d8aeaeb1aaacf86170263859712f272a53da9cfd
Joshua Pangborn <joshua@pangborn.cloud>
2025-08-03 13:37:22 -0400

    Add basic architecture tests

---
+++ tests/Feature/ArchTest.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\Controller;
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notification;
+use Livewire\Form;
+
+arch("globals")
+    ->expect(["dd", "ddd", "die", "dump", "ray", "sleep", "eval", "env"])
+    ->toBeUsedInNothing();
+
+arch("models")->expect("App\Models")->toExtend(Model::class);
+
+arch("controllers")
+    ->expect("App\Controllers")
+    ->toBeClasses()
+    ->toExtend(Controller::class);
+
+arch("livewire forms")
+    ->expect("App\Livewire\Forms")
+    ->toBeClasses()
+    ->toExtend(Form::class);
+
+arch("notifications")
+    ->expect("App\Notifications")
+    ->toBeClasses()
+    ->toExtend(Notification::class);
+
+arch("enums")->expect("App\Enums")->toBeEnums();
+
+arch("commands")
+    ->expect("App\Console\Commands")
+    ->toBeClasses()
+    ->toExtend(Command::class);
+
+arch("jobs")
+    ->expect("App\Jobs")
+    ->toBeClasses()
+    ->toImplement(ShouldQueue::class)
+    ->toHaveMethod("handle");
