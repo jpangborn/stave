b73590a4b61327829b27ad02a2866deaa2f6141f
Joshua Pangborn <joshua@pangborn.cloud>
2025-08-14 19:01:37 -0400

    Place auth file in webroot

--- entrypoint.d/30-composer-install.sh
+++ entrypoint.d/30-composer-install.sh
@@ -2,12 +2,7 @@
 set -e
 
 echo "Setup Composer Private Repo Authentication"
-
-if [ -n "$COMPOSER_AUTH_JSON_BASE64" ]; then
-    mkdir -p ~/.composer
-    echo "$COMPOSER_AUTH_JSON_BASE64" | base64 -d > ~/.composer/auth.json
-    chmod 600 ~/.composer/auth.json
-fi
+echo "$COMPOSER_AUTH_JSON_BASE64" | base64 -d > /var/www/html/auth.json
 
 echo "Install Composer Dependencies"
 cd /var/www/html
