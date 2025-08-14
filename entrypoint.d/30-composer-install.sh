adcc9276b3a5b4ea015a6e003466a2e94d477cef
Joshua Pangborn <joshua@pangborn.cloud>
2025-08-14 18:26:31 -0400

    Add private composer repo to Kamal deployment

--- entrypoint.d/30-composer-install.sh
+++ entrypoint.d/30-composer-install.sh
@@ -3,5 +3,11 @@ set -e
 
 echo "Install Composer Dependencies"
 
+if [ -n "$COMPOSER_AUTH_JSON_BASE64" ]; then
+    mkdir -p /root/.composer
+    echo "$COMPOSER_AUTH_JSON_BASE64" | base64 -d > /root/.composer/auth.json
+    chmod 600 /root/.composer/auth.json
+fi
+
 cd /var/www/html
-composer install
+composer install --no-interaction --prefer-dist --optimize-autoloader
