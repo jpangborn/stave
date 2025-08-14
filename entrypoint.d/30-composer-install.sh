a23140213061c8cfc9fdcff23b8f6458681a62a5
Joshua Pangborn <joshua@pangborn.cloud>
2025-08-14 19:03:52 -0400

    Refactor composer install script

--- entrypoint.d/30-composer-install.sh
+++ entrypoint.d/30-composer-install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
+cd /var/www/html
+
 echo "Setup Composer Private Repo Authentication"
-echo "$COMPOSER_AUTH_JSON_BASE64" | base64 -d > /var/www/html/auth.json
+echo "$COMPOSER_AUTH_JSON_BASE64" | base64 -d > auth.json
+
+cat auth.json
 
 echo "Install Composer Dependencies"
-cd /var/www/html
 composer install --no-interaction --prefer-dist --optimize-autoloader
