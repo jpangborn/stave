06c4ec1cb20eb9bf61c81dc7cfa2e898020d6de5
Joshua Pangborn <joshua@pangborn.cloud>
2025-08-17 09:16:06 -0400

    Adjust migration

--- database/migrations/2025_08_03_171821_add_person_id_and_is_active_to_users_table.php
+++ database/migrations/2025_08_03_171821_add_person_id_and_is_active_to_users_table.php
@@ -15,37 +15,7 @@ return new class() extends Migration
     {
         // Step 1: Add nullable person_id
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('person_id')->unsigned()->default(0)->after('id');
-        });
-
-        // Step 2: Backfill people and update users
-        DB::table('users')
-            ->get()
-            ->each(function ($user) {
-                $personId = DB::table('people')->insertGetId([
-                    'first_name' => explode(' ', $user->name)[0],
-                    'last_name' => explode(' ', $user->name)[1],
-                    'email' => $user->email,
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ]);
-
-                DB::table('users')
-                    ->where('id', $user->id)
-                    ->update(['person_id' => $personId]);
-            });
-
-        // Step 3: Make person_id required and add FK
-        Schema::table('users', function (Blueprint $table) {
-            $table
-                ->foreign('person_id')
-                ->references('id')
-                ->on('people')
-                ->cascadeOnDelete();
-        });
-
-        // Step 4: Add is_active
-        Schema::table('users', function (Blueprint $table) {
+            $table->foreignIdFor(Person::class)->after('id');
             $table->boolean('is_active')->default(true)->after('password');
         });
     }
