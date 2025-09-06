e7c61d6a578c22da9c5394a6f35efac3b2025c7c
Joshua Pangborn <joshua@pangborn.cloud>
2025-09-05 21:31:50 -0400

    Adjust test name for better clarity

--- tests/Browser/SmokeTest.php
+++ tests/Browser/SmokeTest.php
@@ -2,7 +2,7 @@
 
 uses(\Illuminate\Foundation\Testing\RefreshDatabase::class);
 
-it('renders key pages without smoke', function (): void {
+it('renders guest pages without smoke', function (): void {
     $routes = ['/', '/login', '/register'];
 
     $this->visit($routes)->assertNoSmoke();
