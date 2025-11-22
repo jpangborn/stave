0ea1294dbdec884cfea38a3f090dec0ff762c272
Joshua Pangborn <joshua@pangborn.cloud>
2025-11-21 21:10:31 -0500

    Fix missing classes on Song

--- resources/views/livewire/elements/song.blade.php
+++ resources/views/livewire/elements/song.blade.php
@@ -85,7 +85,7 @@ new class extends Component {
                     @endforeach
                 </flux:select>
             </div>
-            <div class="pr-2">
+            <div class="hidden md:block md:pr-2">
                 <flux:dropdown align="end" offset="-15">
                     <flux:button variant="ghost" size="sm" icon="ellipsis-horizontal" inset="bottom" />
 
