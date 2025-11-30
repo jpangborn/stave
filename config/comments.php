df43a8b4db414602322b1340da4fbcbbc8651729
Pint <pint@pangborn.cloud>
2025-11-30 03:34:57 +0000

    Pint formatting

--- config/comments.php
+++ config/comments.php
@@ -9,7 +9,6 @@ use Spatie\Comments\Actions\SendNotificationsForPendingCommentAction;
 use Spatie\Comments\Models\Comment;
 use Spatie\Comments\Models\CommentNotificationSubscription;
 use Spatie\Comments\Models\Reaction;
-use Spatie\Comments\Notifications\ApprovedCommentNotification;
 use Spatie\Comments\Notifications\PendingCommentNotification;
 use Spatie\Comments\Support\CommentSanitizer;
 
