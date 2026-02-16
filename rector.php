9bfbb09651853d0fabe9d8010c034e053aa3dd91
Joshua Pangborn <joshua@pangborn.cloud>
2026-02-15 22:08:46 -0500

    Fix missing import

--- rector.php
+++ rector.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use RectorLaravel\Set\LaravelSetProvider;
 
 return RectorConfig::configure()
     ->withSetProviders(LaravelSetProvider::class)
